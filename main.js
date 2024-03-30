6702c7712b0359421faeae5ea816b7c76fdeae16
Dirgh33 <144554365+Dirgh33@users.noreply.github.com>
2024-03-30 17:26:27 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+    mustache_image = loadImage('https://i.postimg.cc/sXK839F7/mustache-filter-image.png')
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('Posenet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(mustache_image, noseX - 19, noseY, 40, 40);
+}
+
+function take_snapshot(){
+    save('myMustachFilterImage.png');
+}
